8b9868e5efd45886d3cfa74de60178bb31cef117
Amit <amit.silicon2008@gmail.com>
2022-04-26 19:59:39 +0530

    Message updated

--- main.go
+++ main.go
@@ -10,7 +10,7 @@ func main() {
 	http.HandleFunc("/helloworld", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Hello, World!")
 	})
-	fmt.Printf("Server running (port=8080), route: http://localhost:8080/helloworld\n")
+	fmt.Printf("Server running on port=8080, URL: http://localhost:8080/helloworld\n")
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatal(err)
 	}
